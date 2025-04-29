6b727c3efa354d15f1331533b99ad03ade8806f9
Nikolle <yameli082003@gmail.com>
2025-04-29 17:00:28 -0500

    Reajuste en responsible de movil

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,15 @@ export default function Home() {
   const [activeItem, setActiveItem] = useState(0)
 
   return (
-    <div className="grid min-h-screen p-4 md:p-8 pb-20 font-[family-name:var(--font-geist-sans)] text-green-500">
+    <div className="min-h-screen p-4 md:p-8 pb-6 md:pb-20 font-[family-name:var(--font-geist-sans)] text-green-500">
       <main className="flex flex-col md:flex-row w-full row-start-2 h-full items-stretch gap-4">
         {/* En móviles: Nav arriba, cuadrado abajo */}
         {/* En desktop: Nav a la izquierda, cuadrado a la derecha overflow-hidden h-[600px] md:h-[700px*/}
         <div className="w-full md:w-auto">
           <Nav activeItem={activeItem} setActiveItem={setActiveItem} />
         </div>
-        <div className="flex-1 bg-gray-800 rounded-4xl border border-purple-900/50 md:mt-0 md:ml-[50px] overflow-hidden flex h-[calc(100vh-90px)]">
+        <div className="flex-1 bg-gray-800 rounded-4xl border border-purple-900/50 overflow-hidden flex 
+                        h-[calc(100vh-160px)] md:h-[calc(100vh-200px)] md:ml-[50px]">
           <ContentContainer activeItem={activeItem} />
         </div>
       </main>
